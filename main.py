55b8af65411ad990cc65c2d7d32bd2a3fb0e73d4
Sathwikshetty16 <113185195+Sathwikshetty16@users.noreply.github.com>
2023-04-23 21:50:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import keras
-!pip install keras
+# !pip install keras
 import keras.backend as k
 from keras.layers import Conv2D, MaxPooling2D, SpatialDropout2D, Flatten, Dropout, Dense
 from keras.models import Sequential, load_model
